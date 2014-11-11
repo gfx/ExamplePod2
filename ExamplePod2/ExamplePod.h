49e7b25c75b828d7a403bff23c6b738bda0b964c
Fuji, Goro (gfx) <goro-fuji@cookpad.com>
2014-11-11 14:50:26 +0900

    initial commit

---
+++ ExamplePod2/ExamplePod.h
@@ -0,0 +1,19 @@
+//
+//  ExamplePod.h
+//  ExamplePod
+//
+//  Created by goro-fuji on 11/7/14.
+//  Copyright (c) 2014 Cookpad Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExamplePod.
+FOUNDATION_EXPORT double ExamplePodVersionNumber;
+
+//! Project version string for ExamplePod.
+FOUNDATION_EXPORT const unsigned char ExamplePodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExamplePod/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios
+
+source 'git@github.com:gfx/PrivateSpecsExample.git'
+source 'https://github.com/CocoaPods/Specs.git'
+
+pod 'ExamplePod'
